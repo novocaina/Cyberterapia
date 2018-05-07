bdaff30170f6bff8be799af9b91e1ab59a75adbb
Novocaina <mazuralicja96@gmail.com>
2018-05-07 08:55:33 +0200

    Add 2 games

---
+++ Literki/Assets/Hat Trick - The Catch Game/Scripts/HT_Explode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HT_Explode : MonoBehaviour {
+
+	public GameObject explosion;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    
+        {
+		if (other.gameObject.tag == "Hat") {
+			Instantiate (explosion, transform.position, transform.rotation);
+			
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Literki/Assets/Hat Trick - The Catch Game/Scripts/HT_Restart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HT_Restart : MonoBehaviour {
+
+	public void OnMouseDown () {
+        //Application.LoadLevel (Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
